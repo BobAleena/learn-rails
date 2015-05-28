f720940b004ef987d3637346ebb603b16e98303a
bob.aleena@yahoo.com <bob.aleena@yahoo.com>
2015-05-28 01:09:31 -0700

    updated errors for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,14 +22,14 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
-  config.action_mailer.delivery_mehtod = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name
-    authentication: "plain"
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
